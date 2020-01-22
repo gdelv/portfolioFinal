c9b3a22faea0d6f40b8e40070c28d58be613a8d2
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-22 14:42:22 -0500

    completed projects 1.0

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -2,17 +2,37 @@ import React from 'react'
 import { projects } from "../data";
 
 const Projects = () => {
-    return ( 
-    <>
-        <h3>Projects</h3>
-        {projects.map( project => {
-            return (
-                <div>
-        <h3>{project.title}</h3>
-                </div>
-            )
-        })}
-    </>
+    return (
+        <>
+            <h2 className='projects-title' id="projects">Projects</h2>
+            {projects.map(project => {
+                return (
+                    <div className='projects-container'>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <img src={project.img} alt='project' />
+                        <div className='project-buttons-container'>
+                            <form action={project.webLink} target='_blank'>
+                                <button type='submit'>Web Link</button>
+                            </form>
+                            <form action={project.repoLink} target='_blank'>
+                                <button type='submit'>Repo Link</button>
+                            </form>
+                        </div>
+                        <div className='tech-container'>
+                            {project.technologyUsed.map(tech => {
+                                return (
+                                    <div>
+                                        {tech.logo}
+                                        <p>{tech.name}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
