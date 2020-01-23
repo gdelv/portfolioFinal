5fde327478b158819490378b0f9cd6cb49d8a250
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-23 11:37:16 -0500

    mobile version 1.0 complete without fonts

--- src/screens/Container.jsx
+++ src/screens/Container.jsx
@@ -4,7 +4,7 @@ import About from './About'
 import Languages from './Languages'
 import Projects from './Projects'
 import Contact from './Contact'
-
+//state for button to scroll down to "see more"
 class Container extends React.Component {
     state = {
 
