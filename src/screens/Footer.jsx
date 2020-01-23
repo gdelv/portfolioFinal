19324a7fc0ed14660b6f9691dccbc50c0e645206
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-23 12:14:30 -0500

    added fonts and checked for fonts to match styles correctly

--- src/screens/Footer.jsx
+++ src/screens/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <p>Version 1.6 (Last Updated on January 2020)</p>
+            <p>Version 2.6 (Last Updated on January 2020)</p>
         </div>
     )
 }
